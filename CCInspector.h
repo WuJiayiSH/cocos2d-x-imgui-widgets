febc9e9ada6ebd289269959b3f292a1f18549d7d
WuJiayiSH <wujiayish@msn.com>
2024-02-19 19:07:09 +0800

    Added haeder guards

--- CCInspector.h
+++ CCInspector.h
@@ -1,4 +1,9 @@
+#ifndef __CCINSPECTOR_H__
+#define __CCINSPECTOR_H__
+
 namespace CCImGuiWidgets
 {
     void ShowInspector(bool* open);
 }
+
+#endif
--- FGUIInspector.h
+++ FGUIInspector.h
@@ -1,4 +1,9 @@
+#ifndef __FGUIINSPECTOR_H__
+#define __FGUIINSPECTOR_H__
+
 namespace CCImGuiWidgets
 {
     void ShowFGUIInspector(bool* open);
 }
+
+#endif
